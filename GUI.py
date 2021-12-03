548d7a562b2217b8b1221a2700b7607beed5642d
amandajk <44783583+amandajk@users.noreply.github.com>
2021-12-02 18:20:27 -0600

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,14 @@
+# Robert Kembel
+# Amanda La
+# Alberto Arriaga
+
+
+from tkinter import *
+import sqlite3
+
+root = Tk()
+root.title('Car Rental Database')
+root.geometry("500x500")
+
+
+root.mainloop()
