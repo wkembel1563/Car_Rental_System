a060ec6a3b55b3eb0afcdabb3e8e4383bb40f6c8
wkembel1563 <robert.kembel@mavs.uta.edu>
2021-12-03 23:29:15 -0600

    req3 done

--- GUI.py
+++ GUI.py
@@ -4,6 +4,7 @@
 
 from tkinter import *
 from tkinter import ttk
+# from tkcalendar import Calendar
 import sqlite3
 
 # TODO: format phone number input on tab1
@@ -22,7 +23,8 @@ COL = 1
 # MAIN WINDOW SETUP
 root = Tk()
 root.title('Car Rental Database')
-root.geometry("600x300")
+root.geometry("650x400")
+records = []
 
 ############
 # TAB SETUP
@@ -35,8 +37,8 @@ tab_parent = ttk.Notebook(root)   # Notebook method returns a parent tab
 				  # all tabs you click on will be nested in this 'host' tab
 
 # TAB 1
-tab1 = ttk.Frame(tab_parent) 	  	  # initialize as subtabs as part of parent tab
-tab_parent.add(tab1, text='Add Customer') # embed the tabs in the parent and give each a name
+tab1 = ttk.Frame(tab_parent) 	  	  # initialize subtabs as part of parent tab
+tab_parent.add(tab1, text='Add Customer') # embed tabs in parent and give each a name
 
 # TAB 2
 tab2 = ttk.Frame(tab_parent)
@@ -44,7 +46,7 @@ tab_parent.add(tab2, text='Add Vehicle')
 
 # TAB 3
 tab3 = ttk.Frame(tab_parent)
-tab_parent.add(tab3, text='Tab3')
+tab_parent.add(tab3, text='New Rental')
 
 tab_parent.pack(expand=1, fill='both')    # pack the parent to properly display the tabs
 
@@ -59,7 +61,7 @@ print("Connected to DB")
 cursor = database.cursor()
 
 # TODO not working for NULL CustID
-def insert_customer():
+def insert_customer(): 
 
     insertdb = sqlite3.connect('cars.db')
     insertcursor = insertdb.cursor()
@@ -91,6 +93,80 @@ def insert_vehicle():
     # close db connection
     insertdb.close()
 
+
+def check_for_vehicle():
+    global freeVList
+    global freeVEntry
+    global freeVString
+    global records
+    insertdb = sqlite3.connect('cars.db')
+    insertcursor = insertdb.cursor()
+
+    # TODO: calculate quantity 
+    # Get rental dates to check for free cars
+    startD = sDateEntry.get()
+    returnD = rDateEntry.get()
+
+    # Query for cars free on those days 
+    one   = "SELECT V.Description, V.Year, V.VehicleID, V.Type, V.Category " 
+    two   = "FROM VEHICLE V WHERE V.VehicleID NOT IN (SELECT D.VehicleID FROM RENTAL D) OR V.VehicleID IN " 
+    three = "(SELECT R.VehicleID FROM RENTAL R WHERE CASE WHEN R.StartDate <= ? THEN R.ReturnDate <= ? END "
+    four  = "OR R.StartDate >= ?);" 
+    fullquery = one + two + three + four 
+    insertcursor.execute(fullquery, (startD, startD, returnD)) 
+
+    # Update dropdown menu 
+    records = insertcursor.fetchall()
+    for record in records:
+        freeVList.append(record[0])
+    menu = freeVEntry["menu"] 
+    menu.delete(0, "end") 
+    for string in freeVList:
+        menu.add_command(label=string, command=lambda value=string: freeVString.set(value)) 
+
+    # close db connection
+    insertdb.close()
+    
+
+def insert_rental(): 
+    global records
+    global car 
+    insertdb = sqlite3.connect('cars.db')
+    insertcursor = insertdb.cursor()
+
+    # get vehicle id of selection 
+    vid = ""
+    car = freeVString.get()
+    for record in records:
+        if record[0] == car:
+            vid = record[2]
+            break
+            
+	
+    # Insert rental record in db 
+    insertString = "INSERT INTO RENTAL VALUES(:CustID, :VehicleID, :StartDate, :OrderDate, :RentalType, :Qty, :ReturnDate, :TotalAmount, :PaymentDate)"
+    insertcursor.execute(insertString,{
+                            'CustID'     : t3CustID.get(),
+                            'VehicleID'  : vid, 
+                            'StartDate'  : sDateEntry.get(),
+        		    'OrderDate'  : oDateEntry.get(),
+        		    'RentalType' : rentalTypeVar.get(),
+        		    'Qty'        : 0, 
+        		    'ReturnDate' : rDateEntry.get(), 
+        		    'TotalAmount': amntDueEntry.get(), 
+        		    'PaymentDate': pDateEntry.get() 
+    })
+
+    one = "UPDATE RENTAL SET Qty = CASE WHEN RentalType = 1 THEN "
+    two = "(JULIANDAY(ReturnDate) - JULIANDAY(StartDate)) "
+    three = "WHEN RentalType = 7 THEN (JULIANDAY(ReturnDate) - JULIANDAY(StartDate))/7 END;"
+    update = one + two + three
+    insertcursor.execute(update)
+
+    insertdb.commit()
+
+    # close db connection
+    insertdb.close()
 
 
 # GUI components for root window
@@ -178,5 +254,81 @@ catEntry.grid(row  = catEntryPos[ROW],  column = catEntryPos[COL], sticky = W)
 t2submitbutton = Button(tab2, text = 'Add Vehicle', command = insert_vehicle)
 t2submitbutton.grid(row = t2SubmitBtnPos[0], column=t2SubmitBtnPos[1], columnspan = t2SubmitBtnPos[2], pady = t2SubmitBtnPos[3], padx = t2SubmitBtnPos[4], ipadx=t2SubmitBtnPos[5])
 
+#######################################
+# TAB3 GRID POSITIONS - ADD NEW RENTAL 
+t3CustIDlabelPos  = [0,0] # customer id 
+t3CustIDPos       = [0,1]  
+oDateLabelPos  = [1,0]  # order date 
+oDateEntryPos  = [1,1]
+sDateLabelPos  = [2,0]  # start date 
+sDateEntryPos  = [2,1]
+rDateLabelPos  = [3,0]  # return date
+rDateEntryPos  = [3,1]
+t3CheckBtnPos = [4,0,2,10,10,100] # row - col - columnspan - pady - padx - ipadx
+pDateLabelPos  = [5,0]  # payment date
+pDateEntryPos  = [5,1]
+rTypeLabelPos  = [6,0]  # rental type
+rTypeDailyPos  = [6,1]
+rTypeWeeklyPos = [7,1]
+# qtyLabelPos    = [8,0]  # quantity 
+# qtyEntryPos    = [8,1]
+freeVLabelPos  = [8,0]	# display free vehicle choices 
+freeVEntryPos  = [8,1]
+amntDueLabelPos  = [9,0]  # total amount due
+amntDueEntryPos  = [9,1]
+t3SubmitBtnPos = [10,0,2,10,10,100] # row - col - columnspan - pady - padx - ipadx
+
+
+# TAB3 labels/entriesj
+rentalTypeVar = IntVar()  # tracks value of rental type radio button 
+
+t3CustIDlabel = Label(tab3, text = 'Customer ID: ')
+t3CustID      = Entry(tab3, justify=LEFT, width = 30)
+oDateLabel    = Label(tab3, text = 'Order Date: ')
+oDateEntry    = Entry(tab3, justify=LEFT, width = 9) 
+sDateLabel    = Label(tab3, text = 'Start Date: ')
+sDateEntry    = Entry(tab3, justify=LEFT, width = 9)
+rDateLabel    = Label(tab3, text = 'Return Date: ')
+rDateEntry    = Entry(tab3, justify=LEFT, width = 9)
+pDateLabel    = Label(tab3, text = 'Payment Date: ')
+pDateEntry    = Entry(tab3, justify=LEFT, width = 9) 
+rTypeLabel    = Label(tab3, text = 'Rental Type: ')
+rTypeDaily    = Radiobutton(tab3, justify=LEFT, text="Daily", padx = 10, variable=rentalTypeVar, value = 1)
+rTypeWeekly   = Radiobutton(tab3, justify=LEFT, text="Weekly",padx = 10, variable=rentalTypeVar, value = 7)
+# qtyLabel      = Label(tab3, text = 'Quantity (# Days or Weeks): ')
+# qtyEntry      = Entry(tab3, justify=LEFT, width = 5)
+freeVLabel    = Label(tab3, text = 'Free Vehicles: ')
+freeVList     = ["NULL"] 
+freeVString   = StringVar(root)
+freeVString.set("")
+freeVEntry    = OptionMenu(tab3, freeVString, *freeVList)
+amntDueLabel  = Label(tab3, text = 'AmountDue: ')
+amntDueEntry  = Entry(tab3, justify=LEFT, width = 10)
+
+t3CustIDlabel.grid(row = t3CustIDlabelPos[ROW], column = t3CustIDlabelPos[COL], sticky = W)
+t3CustID.grid(row    = t3CustIDPos[ROW],   column = t3CustIDPos[COL],   sticky = W)
+oDateLabel.grid(row  = oDateLabelPos[ROW], column = oDateLabelPos[COL], sticky = W)
+oDateEntry.grid(row  = oDateEntryPos[ROW], column = oDateEntryPos[COL], sticky = W)
+sDateLabel.grid(row  = sDateLabelPos[ROW], column = sDateLabelPos[COL], sticky = W)
+sDateEntry.grid(row  = sDateEntryPos[ROW], column = sDateEntryPos[COL], sticky = W)
+rDateLabel.grid(row  = rDateLabelPos[ROW], column = rDateLabelPos[COL], sticky = W)
+rDateEntry.grid(row  = rDateEntryPos[ROW], column = rDateEntryPos[COL], sticky = W)
+pDateLabel.grid(row  = pDateLabelPos[ROW], column = pDateLabelPos[COL], sticky = W)
+pDateEntry.grid(row  = pDateEntryPos[ROW], column = pDateEntryPos[COL], sticky = W)
+rTypeLabel.grid(row  = rTypeLabelPos[ROW], column = rTypeLabelPos[COL], sticky = W)
+rTypeDaily.grid(row  = rTypeDailyPos[ROW], column = rTypeDailyPos[COL], sticky = W)
+rTypeWeekly.grid(row = rTypeWeeklyPos[ROW], column = rTypeWeeklyPos[COL], sticky = W)
+# qtyLabel.grid(row    = qtyLabelPos[ROW],   column = qtyLabelPos[COL],   sticky = W)
+# qtyEntry.grid(row    = qtyEntryPos[ROW],   column = qtyEntryPos[COL],   sticky = W)     
+freeVLabel.grid(row  = freeVLabelPos[ROW], column = freeVLabelPos[COL], sticky = W)   
+freeVEntry.grid(row  = freeVEntryPos[ROW], column = freeVEntryPos[COL], sticky = W)   
+amntDueLabel.grid(row = amntDueLabelPos[ROW], column = amntDueLabelPos[COL], sticky = W) 
+amntDueEntry.grid(row = amntDueEntryPos[ROW], column = amntDueEntryPos[COL], sticky = W) 
+
+#submit
+t3checkbutton = Button(tab3, text = 'Check for Free Vehicle', command = check_for_vehicle)
+t3checkbutton.grid(row = t3CheckBtnPos[0], column=t3CheckBtnPos[1], columnspan = t3CheckBtnPos[2], pady = t3CheckBtnPos[3], padx = t3CheckBtnPos[4], ipadx=t3CheckBtnPos[5])
+t3submitbutton = Button(tab3, text = 'Create Rental', command = insert_rental)
+t3submitbutton.grid(row = t3SubmitBtnPos[0], column=t3SubmitBtnPos[1], columnspan = t3SubmitBtnPos[2], pady = t3SubmitBtnPos[3], padx = t3SubmitBtnPos[4], ipadx=t3SubmitBtnPos[5])
 
 root.mainloop()
